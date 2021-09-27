7000772b5f375ba97f15fcb1c070434fb1e67e72
cn3085 <cn3085@gmail.com>
2021-09-27 10:43:31 +0900

    jwt exception filter

--- src/main/java/org/youth/api/config/security/JwtAuthenticationFilter.java
+++ src/main/java/org/youth/api/config/security/JwtAuthenticationFilter.java
@@ -12,21 +12,40 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 	
 	private final JwtTokenProvider jwtProvider;
+	
+	public static final String JWT_EXCEPTION = "JWT-EXCEPTION";
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		
 		String token = jwtProvider.resolveToken((HttpServletRequest)request);
 		
-		if(token != null && jwtProvider.validateToken(token)) {
-			Authentication authentication = jwtProvider.getAuthentication(token);
-			SecurityContextHolder.getContext().setAuthentication(authentication);
+		try {
+			if(token != null && jwtProvider.validateToken(token)) {
+				Authentication authentication = jwtProvider.getAuthentication(token);
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+		}catch(ExpiredJwtException e) {
+			request.setAttribute(JWT_EXCEPTION, "로그인 세션이 만료되었습니다. 새로운 로그인이 필요합니다.");
+		}catch(UnsupportedJwtException e) { //예상하는 형식과 다른 jwt토큰
+			request.setAttribute(JWT_EXCEPTION, "로그인 형식이 올바르지 않습니다.");
+		}catch (MalformedJwtException e) { //jwt 구성이 올바르지 않음
+			request.setAttribute(JWT_EXCEPTION, "로그인 구성이 올바르지 않습니다.");
+		}catch (SignatureException e) { //기존 서명이 확인되지 않음
+			request.setAttribute(JWT_EXCEPTION, "기존의 로그인 토큰이 확인되지 않습니다.");
+		}catch(Exception e) {
+			request.setAttribute(JWT_EXCEPTION, "로그인 오류");
 		}
 		
 		chain.doFilter(request, response);
