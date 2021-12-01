4e934c77db210b21f303466d01bb217610a12755
cn3085 <cn3085@gmail.com>
2021-12-02 08:32:18 +0900

    로그인 요청 허용 / 브라우저라우터 가능하도록 추가

---
+++ src/main/java/org/youth/api/controller/RouterController.java
@@ -0,0 +1,20 @@
+package org.youth.api.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RouterController implements ErrorController {
+	
+	@GetMapping({"/", "/error"})
+	public String index() {
+		return "index.html";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+
+}
